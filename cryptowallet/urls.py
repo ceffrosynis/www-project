ad41cfc84d26d634f88bbbbb1475ada7d20c705d
Charalampos Tsiagkalis <ctsiagkalis@uth.gr>
2019-12-26 20:35:46 +0200

    search.html style

--- cryptowallet/urls.py
+++ cryptowallet/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import profile, index, user
+from .views import profile, index
 
 from . import views
 
@@ -8,5 +8,5 @@ app_name = 'crypto'
 urlpatterns = [
     path('', index.as_view(), name='index'),
     path('profile/', profile.as_view(), name='profile'),
-    path('user/<slug>', user, name='visit_profile'),
+    # path('user/<slug>', user, name='visit_profile'),
 ]
